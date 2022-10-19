43dd6c0a1b26426326234baa165a644722b7c1c6
Pavel <p.gavrilenko@scloud.ru>
2022-10-19 22:18:09 +0300

    tags to viewset

--- backend/tags/urls.py
+++ backend/tags/urls.py
@@ -1,9 +1,17 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from .views import TagList, TagRetrieve
+from .views import TagList, TagRetrieve, TagView
 
+
+# urlpatterns = [
+#     path('tags/', TagList.as_view()),
+#     path('tags/<int:pk>/', TagRetrieve.as_view()),
+# ]
+
+router = DefaultRouter()
+router.register('tags', TagView, basename='tags')
 
 urlpatterns = [
-    path('tags/', TagList.as_view()),
-    path('tags/<int:pk>/', TagRetrieve.as_view()),
+    path('', include(router.urls)),
 ]
--- backend/tags/views.py
+++ backend/tags/views.py
@@ -1,5 +1,5 @@
 from rest_framework.permissions import AllowAny
-from rest_framework import generics
+from rest_framework import generics, viewsets
 
 from .models import Tag
 from .serializers import TagSerializer
@@ -16,6 +16,13 @@ class TagRetrieve(generics.RetrieveAPIView):
     serializer_class = TagSerializer
     queryset = Tag.objects.all()
     permission_classes = [AllowAny, ]
+
+
+class TagView(viewsets.ReadOnlyModelViewSet):
+    serializer_class = TagSerializer
+    queryset = Tag.objects.all()
+    permission_classes = [AllowAny, ]
+    pagination_class = None
 
 
 
