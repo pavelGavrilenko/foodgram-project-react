8732dcab96de3b29f6b33418e4fe71e9a76577e9
Pavel <p.gavrilenko@scloud.ru>
2022-10-20 21:45:33 +0300

    правка айсорта

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import (Ingredient, Recipe, Favorite,
-                     IngredientAmount, ShoppingList)
+from .models import (Favorite, Ingredient, IngredientAmount,
+                     Recipe, ShoppingList)
 
 
 class IngredientAdmin(admin.ModelAdmin):
